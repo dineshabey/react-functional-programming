fe48ad3146e2500b554febc07ad728b09b11ca7f
Dinesh <dinabeysinghe@gmail.com>
2023-11-23 20:31:34 +0530

    add funtions call basic part 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+function myFunction() {
+  return function () {
+    console.log("My Function CLG");
+  }
+}
+
+// const result = myFunction();
+// console.log(typeof result);
+// result();
+
+function addNumbers(a, b) {
+  return (a + b);
+};
+
+// const result = addNumbers(10, 20);
+
+// console.log(result);
+
+// myFunction()();
+
+function add(a) {
+  return function () {
+    return (a + 10);
+  }
+}
+
+const result = add(5)();
+console.log(result);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
     </div>
   );
 }
